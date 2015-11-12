e090900bb6c8834edfe2efc7c9190b1903688785
Aaper <aram.parsegyan@gmail.com>
2015-11-12 21:58:11 +0100

    broad back instagram login, added functions for color, fixed text bug, broad trends of instagram to the trend board

--- js/FeedController.js
+++ js/FeedController.js
@@ -1,5 +1,9 @@
 app.controller('FeedController', ['$scope', 'DataService', function($scope, DataService) {
+    $scope.image = "image";
+    $scope.video = "video";
+    $scope.youtube = "https://youtu.be/gHonBC3naj4";
     $scope.feed = DataService.getPosts();
+    console.log($scope.feed);
     }])
     .directive('feedView', function() {
         return {
@@ -7,7 +11,7 @@ app.controller('FeedController', ['$scope', 'DataService', function($scope, Data
             template: '<div class="social-media-content-container-feed box-shadow" ng-repeat="post in' +
             ' feed">' +
             '<div class="social-media-content-header-feed">'+
-                '<div class="social-media-content-title-feed">Autor des Beitrags</div>'+
+                '<div class="social-media-content-title-feed"><img src="{{post.authorPicture}}" style="width:5vh;height:5vh;"> {{post.author}}</div>'+
              '<div class="social-media-content-header-x-feed box-shadow"  style="background-color:{{post.socialNetwork.color}};">'+
                             '<i class="fa fa-{{post.socialNetwork.icon}} center"></i>'+
                         '</div>'+
@@ -15,9 +19,12 @@ app.controller('FeedController', ['$scope', 'DataService', function($scope, Data
                         '</div>'+
                         '<div class="social-media-content-body-feed">'+
         '<div class="social-media-content-body-entry-image-feed">'+
-            '<div class="center" style="font-size:7vh;"><i class="fa fa-picture-o"></i></div>'+
+            '<div class="center" style="font-size:7vh;">'+
+            '<img ng-if="post.type === image" src="{{post.media}}" style="max-height:9vw; max-width:13vw;" />'+
+            '<video ng-if="post.type === video" controls style="max-height:9vw; max-width:13vw;"><source ng-src="{{youtube}}"></source></video>'+
+            '</div>'+
         '</div>'+
-                    '<div class="social-media-content-body-entry-text-feed"><div class="center">{{post.text}}</div></div></div>'+
+                    '<div class="social-media-content-body-entry-text-feed" style="overflow:hidden;"><div class="center" style="width:13vw; height:3vw; overflow:hidden;">{{post.text}}<br/>Likes: {{post.numberOfLikes}}</div></div></div>'+
                     '</div>'
             //templateUrl: 'FeedView.html'
         };
