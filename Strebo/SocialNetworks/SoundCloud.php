9330eed2ddda15c46fd4d627d506f3f7644eea8b
Aaper <aram.parsegyan@gmail.com>
2015-11-18 11:12:01 +0100

    code cleanup, added time formatter

--- Strebo/SocialNetworks/SoundCloud.php
+++ Strebo/SocialNetworks/SoundCloud.php
@@ -4,8 +4,6 @@ use Strebo;
 
 require __DIR__.'/../AbstractSocialNetwork.php';
 
-use MetzWeb\Instagram\Instagram as InstagramAPI;
-
 class SoundCloud extends Strebo\AbstractSocialNetwork implements Strebo\PublicInterface{
 
 public function __construct(){
@@ -18,7 +16,7 @@ public function connect($code){
 
 public function getPersonalFeed(){
 	
-	$feed=$this->instagram->getUserFeed(35);
+	$feed;
 	return $this->encodeJSON($feed);
 	
 }
@@ -57,7 +55,7 @@ public function encodeJSON($json){
 		$temp_song["numberOfLikes"] = $song["likes_count"];
 		$temp_song["link"] = $song["permalink_url"];
 		$temp_song["type"] = "audio";
-		$temp_song["createdTime"] = $song["created_at"];
+		$temp_song["createdTime"] = $this->formatTime($song["created_at"]);
 		$temp_song["media"] = $song["stream_url"] . '?client_id=d08c99a67fa0518806f5fe1f4bf36792';
 		$temp_song["thumb"] = $song["artwork_url"];
 		$feed[$i] = $temp_song;
@@ -72,6 +70,20 @@ public function encodeJSON($json){
 
 	return json_encode($newJSON);
 }
+
+public function formatTime($time)
+    {
+        $timeJSON = array('day' => substr($time, 8, 2),
+            'month' => substr($time, 5,2),
+            'year' => substr($time, 0,4),
+            'hour' => substr($time, 11, 2),
+            'minute' => substr($time, 14, 2),
+            'second' => substr($time, 17, 2)
+        );
+
+        return json_encode($timeJSON);
+
+    }
 }
 
 ?>
