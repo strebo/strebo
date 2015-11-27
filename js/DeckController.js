7a10c092be390bd203ecde16708b0ff8d946c25b
ScientiaEtVeritas <fabian_retkowski@live.de>
2015-11-27 21:26:08 +0100

    design improvments detail view / deck view popup functionality

--- js/DeckController.js
+++ js/DeckController.js
@@ -1,5 +1,14 @@
 app.controller('DeckController', ['$scope', 'DataService', function($scope, DataService) {
         $scope.networks = DataService.getPostsByNetwork();
+        $scope.showDetailView = function(post, socialNetwork) {
+            console.log(post);
+            console.log(socialNetwork);
+            post.socialNetwork = {};
+            post.socialNetwork.color = socialNetwork.color;
+            post.socialNetwork.icon = socialNetwork.icon;
+            $scope.$emit('setCurrentItem', post);
+            $scope.$emit('setDetailView', true);
+        };
     }])
     .directive('deckView', function() {
         return {
