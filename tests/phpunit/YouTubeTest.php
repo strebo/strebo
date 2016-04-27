7ed5ce6a1b258319dab35d57f6d4ade09069b6f1
Aaper <aram.parsegyan@gmail.com>
2016-04-27 12:21:13 +0200

    implemented tests for youtube

--- tests/phpunit/YouTubeTest.php
+++ tests/phpunit/YouTubeTest.php
@@ -4,15 +4,16 @@
  * @coversDefaultClass Strebo\SocialNetworks\YouTube
  */
 
-require __DIR__ .'/../../vendor/autoload.php';
-require __DIR__ .'/../../Autoloader.php';
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../../Autoloader.php';
 
-spl_autoload_register (array ('Autoloader', 'autoload'));
+spl_autoload_register(array('Autoloader', 'autoload'));
 
 class YouTubeTest extends PHPUnit_Framework_TestCase
 {
 
-    public function testConnect(){
+    public function testConnect()
+    {
         // TO BE DONE
     }
 
@@ -20,23 +21,24 @@ class YouTubeTest extends PHPUnit_Framework_TestCase
     {
         putenv('strebo_youtube_1=AIzaSyAH6n3wcnku2Ah3qZZrgWbXcxVgiAwF-Xk');
         putenv('strebo_youtube_2=AIzaSyA8OMzoY6nuaQyQyp3nDSqVpMbjL6juT8U');
-        $youtube=new \Strebo\SocialNetworks\YouTube();
-        $result=json_decode($youtube->getPublicFeed([null,null]));
-        $this->assertEquals("YouTube",$result->name);
-        $this->assertEquals("youtube",$result->icon);
-        $this->assertEquals("#2a5b83",$result->color);
-        $this->assertObjectHasAttribute("type",$result->feed[0]);
-        $this->assertObjectHasAttribute("tags",$result->feed[0]);
-        $this->assertObjectHasAttribute("title",$result->feed[0]);
-        $this->assertObjectHasAttribute("createdTime",$result->feed[0]);
-        $this->assertObjectHasAttribute("link",$result->feed[0]);
-        $this->assertObjectHasAttribute("author",$result->feed[0]);
-        $this->assertObjectHasAttribute("authorPicture",$result->feed[0]);
-        $this->assertObjectHasAttribute("numberOfLikes",$result->feed[0]);
-        $this->assertObjectHasAttribute("media",$result->feed[0]);
+        $youtube = new \Strebo\SocialNetworks\YouTube();
+        $result = json_decode($youtube->getPublicFeed(null));
+        $this->assertEquals("YouTube", $result->name);
+        $this->assertEquals("youtube", $result->icon);
+        $this->assertEquals("#e62117", $result->color);
+        $this->assertObjectHasAttribute("type", $result->feed[0]);
+        $this->assertObjectHasAttribute("tags", $result->feed[0]);
+        $this->assertObjectHasAttribute("title", $result->feed[0]);
+        $this->assertObjectHasAttribute("createdTime", $result->feed[0]);
+        $this->assertObjectHasAttribute("link", $result->feed[0]);
+        $this->assertObjectHasAttribute("author", $result->feed[0]);
+        $this->assertObjectHasAttribute("authorPicture", $result->feed[0]);
+        $this->assertObjectHasAttribute("numberOfLikes", $result->feed[0]);
+        $this->assertObjectHasAttribute("media", $result->feed[0]);
     }
 
-    public function testGetPersonalFeed(){
+    public function testGetPersonalFeed()
+    {
         // TO BE DONE
     }
 
@@ -45,23 +47,23 @@ class YouTubeTest extends PHPUnit_Framework_TestCase
         // TO BE DONE
     }
 
-    public function testEncodeJSON(){
+    public function testEncodeJSON()
+    {
         // TO BE DONE
     }
 
-    public function testFormatTime(){
+    public function testFormatTime()
+    {
         putenv('strebo_youtube_1=AIzaSyAH6n3wcnku2Ah3qZZrgWbXcxVgiAwF-Xk');
         putenv('strebo_youtube_2=AIzaSyA8OMzoY6nuaQyQyp3nDSqVpMbjL6juT8U');
-        $youtube=new \Strebo\SocialNetworks\YouTube();
-        $time=json_decode($youtube->formatTime("1296656006"));
+        $youtube = new \Strebo\SocialNetworks\YouTube();
+        $time = json_decode($youtube->formatTime("2016-04-25T13:00:09.000Z"));
 
-        $this->assertEquals("02",$time->day);
-        $this->assertEquals("02",$time->month);
-        $this->assertEquals("2011",$time->year);
-        $this->assertEquals("15",$time->hour);
-        $this->assertEquals("13",$time->minute);
-        $this->assertEquals("26",$time->second);
+        $this->assertEquals("25", $time->day);
+        $this->assertEquals("04", $time->month);
+        $this->assertEquals("2016", $time->year);
+        $this->assertEquals("15", $time->hour);
+        $this->assertEquals("00", $time->minute);
+        $this->assertEquals("09", $time->second);
     }
-}
-
 }
