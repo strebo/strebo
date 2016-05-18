79eaf985b7bcf9d86cbc71b3b5f8240e2bfd7352
Fabian Retkowski <fabian.retkowski@sap.com>
2016-05-18 17:15:08 +0200

    Set Cookie with ID

---
+++ js/Url.js
@@ -0,0 +1,16 @@
+var Url = {
+    get get() {
+        var vars = {};
+        if (window.location.search.length !== 0)
+            window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+                key = decodeURIComponent(key);
+                if (typeof vars[key] === "undefined") {
+                    vars[key] = decodeURIComponent(value);
+                }
+                else {
+                    vars[key] = [].concat(vars[key], decodeURIComponent(value));
+                }
+            });
+        return vars;
+    }
+};
